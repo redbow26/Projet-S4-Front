c5c15f800f1833b9b9459f5ca812b2648c27c800
Zeni Baptiste <baptiste_zeni@ens.univ-artois.fr>
2021-03-30 16:47:42 +0200

    #7 Fin du trie par nom

--- src/app/_services/jeux.service.ts
+++ src/app/_services/jeux.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {Observable, of, throwError} from 'rxjs';
-import {UserInfo} from '../_models/user-info';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {catchError, map} from 'rxjs/operators';
@@ -24,5 +23,17 @@ export class JeuxService {
         map(rep => rep.data.item),
         catchError(err => throwError(err))
       );
+  }
+
+  triJeuxNom(items: Jeux[], sort?: number): Jeux[] {
+    const itemsCopie = [...items];
+    if (sort === undefined) {return items ; }
+    if (sort > 0) {return itemsCopie.sort((x: Jeux, y: Jeux): number => x.nom > y.nom ? 1 : -1); }
+  }
+
+  triJeuxTheme(items: Jeux[], sort?: number): Jeux[] {
+    const itemsCopie = [...items];
+    if (sort === undefined) {return items ; }
+    if (sort > 0) {return itemsCopie.sort((x: Jeux, y: Jeux): number => x.theme > y.theme ? 1 : -1); }
   }
 }
