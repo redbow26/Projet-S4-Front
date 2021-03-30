83606e5cad5371341b32049781ae82bfba2750e5
Redbow <tristanleroy26@gmail.com>
2021-03-30 23:23:00 +0200

    Issue RO

--- src/app/lp-solver-test/lp-solver-test.component.ts
+++ src/app/lp-solver-test/lp-solver-test.component.ts
@@ -10,6 +10,9 @@ declare var solver: any;
 })
 export class LpSolverTestComponent implements OnInit {
 
+  constructor(public messageService: MessageService) {
+  }
+
 
   readonly probleme = {
     variables: {
@@ -37,8 +40,50 @@ export class LpSolverTestComponent implements OnInit {
     optimize: 'benefice'
   };
 
-  constructor(public messageService: MessageService) {
-  }
+  readonly sacAdoc = {
+    variables: {
+      O1: {
+        weight: 12,
+        profit: 10
+      },
+      O2: {
+        weight: 11,
+        profit: 10
+      },
+      O3: {
+        weight: 7,
+        profit: 15
+      },
+      O4: {
+        weight: 25,
+        profit: 32
+      },
+      O5: {
+        weight: 10,
+        profit: 7
+      },
+      O6: {
+        weight: 5,
+        profit: 7
+      },
+    },
+    ints: {},
+    binaries: {
+      O1: 1,
+      O2: 1,
+      O3: 1,
+      O4: 1,
+      O5: 1,
+      O6: 1
+    },
+    constraints: {
+      weight: {
+        max: 40
+      }
+    },
+    opType: 'max',
+    optimize: 'profit'
+  };
 
   ngOnInit(): void {
   }
@@ -54,6 +99,28 @@ export class LpSolverTestComponent implements OnInit {
       key: 'main',
       severity: 'info',
       detail: `${affiche}`,
+    });
+  }
+
+
+  resolutionSacADos(): void{
+    const resultat = solver.Solve(this.sacAdoc);
+    console.log(resultat);
+    let message = '[';
+    if (resultat.O1) { message += 'Objet 1,'; }
+    if (resultat.O2) { message += 'Objet 2,'; }
+    if (resultat.O3) { message += 'Objet 3,'; }
+    if (resultat.O4) { message += 'Objet 4,'; }
+    if (resultat.O5) { message += 'Objet 5,'; }
+    if (resultat.O6) { message += 'Objet 6,'; }
+
+    message = message.slice(0, -1);
+    message += `] Profit: ${resultat.result}`;
+
+    this.messageService.add({
+      key: 'main',
+      severity: 'info',
+      detail: message,
     });
   }
 
